3fd17b7e0b1b5517b26c31d1fd1411af5636490b
Mark Heines <friteuse@arcor.de>
2025-06-10 10:37:32 +0200

    store user prompts in list

--- main.py
+++ main.py
@@ -2,17 +2,29 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
 
 def main():
-    if len(sys.argv) < 2:
-        print('ERROR: no prompt provided.')
-        sys.exit(1)
     load_dotenv()
+
+    args = sys.argv[1:]
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+    user_prompt = " ".join(args)
+
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)])]
+
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=f'sys.argv[1]'
+        model = 'gemini-2.0-flash-001', 
+        contents = messages,
     )
     print(response.text)
     print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
